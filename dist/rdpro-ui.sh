82fd80612bb216e04a656147660dd7732dc217eb
I831964 <zhongyuan.xue@sap.com>
2018-10-29 16:14:45 -0400

    default the jar location to ~/bin/rdpro

--- dist/rdpro-ui.sh
+++ dist/rdpro-ui.sh
@@ -1,1 +1,1 @@
-java -jar ./rdpro-ui.jar  "$1" "$2" "$3" "$4" "$5"
+java -jar ~/bin/rdpro/rdpro-ui.jar  "$1" "$2" "$3" "$4" "$5"
--- dist/rdpro.sh
+++ dist/rdpro.sh
@@ -1,1 +1,1 @@
-java -jar ./rdpro-console.jar  "$1" "$2" "$3" "$4" "$5"
+java -jar ~/bin/rdpro/rdpro-console.jar  "$1" "$2" "$3" "$4" "$5"
